34019a2f799fc93222b1f516ff083d05522181aa
Talita Groppo <talitagroppo@hotmail.com>
2021-11-15 14:19:34 +0100

    Create Main with buttons and implement in View Controller with action and outlets.

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var image: UIImage?
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var albunsButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var senderImage: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    @IBAction func pickerImage(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+        
+    }
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+           let imagePicker = UIImagePickerController()
+           imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+           present(imagePicker, animated: true, completion: nil)
+       }
+    @IBAction func senderImage(_ sender: Any) {
+        let image = UIImage()
+        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        present(controller, animated: true, completion: nil)
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        if let image = self.image {
+            self.imageView.image = UIImage(named: "d\(image)")
+        } else {
+            self.imageView.image = nil
+        }
+        self.imageView.alpha = 0
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 1.0) {
+            self.imageView.alpha = 1
+            
+        }
+    }
 
 }
 
