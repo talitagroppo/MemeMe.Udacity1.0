b567360b8c823be072a09aaccd4ba215535c4a83
Talita Groppo <talitagroppo@hotmail.com>
2021-11-17 13:27:30 +0100

    Image appearing on the View.

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -6,10 +6,10 @@
 //
 
 import UIKit
+import PhotosUI
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    var imageView: UIImage?
     
     @IBOutlet weak var imageEditor: UIImageView!
     @IBOutlet weak var albunsButton: UIBarButtonItem!
@@ -19,22 +19,40 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var bottomTextField: UITextField!
     @IBOutlet weak var cancel: UIBarButtonItem!
     
+    var itemProviders: [NSItemProvider] = []
+    var interactor: IndexingIterator<[NSItemProvider]>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+      
     }
     @IBAction func pickAnImageFromCamera(_ sender: Any) {
            let imagePicker = UIImagePickerController()
            imagePicker.delegate = self
         imagePicker.sourceType = .camera
            present(imagePicker, animated: true, completion: nil)
-        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
        }
+    @IBAction func pickAnLibraryImage(_ sender: Any) {
+        let photoLibrary = PHPhotoLibrary.shared()
+        var configuration = PHPickerConfiguration(photoLibrary: photoLibrary)
+        configuration.selectionLimit = 0
+//        configuration.filter = .images
+//        configuration.filter = .any(of: [.videos, .livePhotos])
+        let pickerImage = PHPickerViewController(configuration: configuration)
+        pickerImage.delegate = self
+        present(pickerImage, animated: true, completion: nil)
+        
+    }
     @IBAction func senderImage(_ sender: Any) {
         let image = UIImage()
         let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
         present(controller, animated: true, completion: nil)
     }
     
+    @IBAction func cancelButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
     @IBAction func topText(_ sender: Any) {
         let memeTextAttributes: [NSAttributedString.Key: Any] = [
             NSAttributedString.Key.strokeColor: UIColor.white,
@@ -48,3 +66,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
 }
 
+extension ViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        dismiss(animated: true, completion: nil)
+        let identifiars: [String] = results.compactMap(\.assetIdentifier)
+        let _ = PHAsset.fetchAssets(withLocalIdentifiers: identifiars, options: nil)
+        if let provideImage = results.first?.itemProvider, provideImage.canLoadObject(ofClass: UIImage.self){
+        let firstImage = imageEditor.image
+        provideImage.loadObject(ofClass: UIImage.self) { [weak self] image, error in
+            DispatchQueue.main.async {
+                guard let self = self, let image = image as? UIImage, self.imageEditor.image == firstImage else {return}
+                self.imageEditor.image = image
+            }
+
+            }
+        }
+    }
+}
