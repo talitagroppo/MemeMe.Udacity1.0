389becd4836ccee42446a292f876ef250dd7c094
Talita Groppo <talitagroppo@hotmail.com>
2021-11-19 15:36:51 +0100

    FInish

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -10,16 +10,14 @@ import PhotosUI
 import NotificationCenter
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     @IBOutlet weak var imageEditor: UIImageView!
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
     @IBOutlet weak var albunsButton: UIBarButtonItem!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var senderImage: UIBarButtonItem!
-    @IBOutlet weak var topToolBar: UIToolbar!
-    @IBOutlet weak var bottomToolBar: UIToolbar!
-    @IBOutlet weak var save: UIBarButtonItem!
+    @IBOutlet weak var cancel: UIBarButtonItem!
     
     var itemProviders: [NSItemProvider] = []
     var interactor: IndexingIterator<[NSItemProvider]>?
@@ -27,7 +25,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func viewDidLoad() {
         super.viewDidLoad()
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
-        }
+        senderImage.isEnabled = false
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         subscribeToKeyboardNotifications()
@@ -38,62 +37,61 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     @IBAction func pickAnImageFromCamera(_ sender: Any) {
-           let imagePicker = UIImagePickerController()
-           imagePicker.delegate = self
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
         imagePicker.sourceType = .camera
-           present(imagePicker, animated: true, completion: nil)
-       }
+        present(imagePicker, animated: true, completion: nil)
+    }
     @IBAction func pickAnLibraryImage(_ sender: Any) {
         let photoLibrary = PHPhotoLibrary.shared()
         var configuration = PHPickerConfiguration(photoLibrary: photoLibrary)
         configuration.selectionLimit = 0
-//        configuration.filter = .images
-//        configuration.filter = .any(of: [.videos, .livePhotos])
         let pickerImage = PHPickerViewController(configuration: configuration)
         pickerImage.delegate = self
         present(pickerImage, animated: true, completion: nil)
     }
     @IBAction func senderImage(_ sender: Any) {
         let image = UIImage()
-        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        let textField = UITextField()
+        let controller = UIActivityViewController(activityItems: [image, textField], applicationActivities: nil)
         present(controller, animated: true, completion: nil)
     }
-    @IBAction func save(_ sender: Any) {
-        let _ = Meme(topText: topTextField.text!, bottomText: bottomTextField.text!, originalImage: imageEditor.image!)
+    @IBAction func textFieldDidBeginEditing(_ textField: UITextField) {
+        let textField: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.white,
+            NSAttributedString.Key.foregroundColor: UIColor.black,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedString.Key.strokeWidth:  5
+        ]
+        topTextField.defaultTextAttributes = textField
+        bottomTextField.defaultTextAttributes = textField
+    }
+    @IBAction func cancel(_ sender: Any) {
+        self.topTextField.text = "Top"
+        self.bottomTextField.text = "Bottom"
+        self.imageEditor.image = nil
     }
 }
 
 extension ViewController: PHPickerViewControllerDelegate {
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-        hideToolbar(true)
+        senderImage.isEnabled = true
         dismiss(animated: true, completion: nil)
         let identifiars: [String] = results.compactMap(\.assetIdentifier)
         let _ = PHAsset.fetchAssets(withLocalIdentifiers: identifiars, options: nil)
         if let provideImage = results.first?.itemProvider,
            provideImage.canLoadObject(ofClass: UIImage.self){
-        let firstImage = imageEditor.image
+            let firstImage = imageEditor.image
             provideImage.loadObject(ofClass: UIImage.self) { [weak self] image, error in
-            DispatchQueue.main.async {
-                guard let self = self, let image = image as? UIImage, self.imageEditor.image == firstImage else {return}
-                self.imageEditor.image = image
-            }
+                DispatchQueue.main.async {
+                    guard let self = self, let image = image as? UIImage, self.imageEditor.image == firstImage else {return}
+                    self.imageEditor.image = image
+                }
             }
         }
-        hideToolbar(false)
     }
 }
-extension ViewController: UITextFieldDelegate{
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        textField.delegate = self
-        let textField: [NSAttributedString.Key: Any] = [
-            NSAttributedString.Key.strokeColor: UIColor.white,
-            NSAttributedString.Key.foregroundColor: UIColor.black,
-            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-            NSAttributedString.Key.strokeWidth:  15
-        ]
-        topTextField.defaultTextAttributes = textField
-        bottomTextField.defaultTextAttributes = textField
-    }
+extension ViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
@@ -112,24 +110,5 @@ extension ViewController: UITextFieldDelegate{
     func unsubscribeToKeyboardNotifications() {
         NotificationCenter.default.removeObserver(self, name: UIWindow.keyboardWillShowNotification, object: nil)
     }
-    func hideToolbar(_ visibility: Bool) {
-        if !visibility{
-            topToolBar.isHidden = true
-            bottomToolBar.isHidden = true
-        } else if visibility {
-            topToolBar.isHidden = false
-            bottomToolBar.isHidden = false
-        }
-    }
 }
-class Meme {
-    var topText = "TopText"
-     var bottomText = "BottomText"
-     var originalImage = UIImage()
-    
-    init(topText: String, bottomText: String, originalImage: UIImage){
-        self.topText = topText
-        self.bottomText = bottomText
-        self.originalImage = originalImage
-    }
-}
+
